c9a715726fe33f35e07c8cfa4aa6469b44a3d7ac
Paul Ashour <paul.ashour@gmailcom>
2019-02-25 15:52:22 -0700

    inital migrate on user and game

--- db/migrate/20190225223318_create_games.rb
+++ db/migrate/20190225223318_create_games.rb
@@ -1,7 +1,8 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-
+      t.string :name
+      t.integer :user_id
       t.timestamps null: false
     end
   end
