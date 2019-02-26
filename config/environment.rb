247c6a24400ebaceb549c382dde301fdbc271b2c
github username <github email address>
2019-02-25 23:17:11 -0700

    continue to add postgres (not yet working)

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
+  :adapter => "postgresql",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
