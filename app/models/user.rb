2d51231910b5c0afea43e2831214ee1f7f7046f5
github username <github email address>
2019-03-03 17:25:23 -0700

    Add game belongs_to user/user has_many game association.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates_presence_of :username, :email, :password_digest
   has_secure_password
+  has_many :game
 end
---
+++ db/migrate/20190304002404_add_userid_to_games.rb
@@ -0,0 +1,5 @@
+class AddUseridToGames < ActiveRecord::Migration
+  def change
+    add_column :games, :user_id, :integer
+  end
+end
