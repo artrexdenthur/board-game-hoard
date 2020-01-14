83371860f8f1ff9df6f3bac7e8acbaa9539c8f1e
Paul Ashour <paul.ashour@gmail.com>
2020-01-13 19:45:13 -0700

    Update seeds and change index to round avg rating

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,26 @@ require 'faker'
 User.destroy_all
 Game.destroy_all
 
-10.times do |i|
+names = [
+  "Agricola",
+  "Twilight Imperium",
+  "Caverna",
+  "Codenames",
+  "Betrayal at House on the Hill",
+  "Parchisi",
+  "Love Letter",
+  "Bang",
+  "Race for the Galaxy",
+  "Decrypto",
+  "Fortress America"
+]
+
+9.times do |i|
   user = User.create(username: Faker::Internet.username,
     email: Faker::Internet.safe_email,
     password: "pass123"
   )
-  Game.create(name: Faker::Space.moon, user_id: user.id)
-  Game.create(name: Faker::Verb.base, user_id: user.id)
+  names.each do |n|
+    Game.create(name: n, user_id: user.id, rating: (rand*10).round + 1, plays: (rand*100).round + 1)
+  end
 end
